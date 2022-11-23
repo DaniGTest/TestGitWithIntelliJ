aee979e74e54693dc6023b244377b8a28ee2afdf
Dani <dani.georgieva.bg@gmail.com>
2022-11-23 14:30:42 +0200

    update test hello world program

--- src/main/java/JustTestsWithGit/TestHelloWorld.java
+++ src/main/java/JustTestsWithGit/TestHelloWorld.java
@@ -4,5 +4,6 @@ public class TestHelloWorld {
     public static void main(String[] args) {
         System.out.println("Test Hello World!");
         System.out.println("Test Hello World 2!");
+
     }
 }
