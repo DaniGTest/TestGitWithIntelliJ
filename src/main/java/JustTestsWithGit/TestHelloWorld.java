92721107d81accf56eaa6056b3cfe5fe2efbb7a2
Dani <dani.georgieva.bg@gmail.com>
2022-11-23 14:24:32 +0200

    initial commit test

--- src/main/java/JustTestsWithGit/TestHelloWorld.java
+++ src/main/java/JustTestsWithGit/TestHelloWorld.java
@@ -3,5 +3,6 @@ package JustTestsWithGit;
 public class TestHelloWorld {
     public static void main(String[] args) {
         System.out.println("Test Hello World!");
+        System.out.println("Test Hello World 2!");
     }
 }
